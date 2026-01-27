f54bb40bbee2ba2abc3663f4b1e64ebc6934675a
sonjinile <sy7521012@naver.com>
2026-01-27 20:18:37 +0900

    2026-01-27 at 20:18 React Study

--- blog/src/App.js
+++ blog/src/App.js
@@ -1,23 +1,42 @@
+/* eslint-disable */
+
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  
+  let post ='강남 우동 맛집';
+  let [글제목, set글제목] = useState(['남자 코트 추천', '강남 우동맛집', '파이썬독학']);
+  let [logo, setLogo] = useState('ReactBlog');
+
+  let [thumb, setThumb] = useState(0);
+
+  let num = [1,2,3];
+
+  let [a, c] = [1, 2];
+
+  // let a = num[0];
+  // let c = num[1];
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Let's Do<code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div style={{background: 'black', color: 'red'}}>
+        <h4>{logo}</h4>
+      </div>
+      <div className='list'>
+        <h4>{글제목[0]} <span onClick={() => setThumb(thumb + 1)}>👍</span> {thumb} </h4>
+        <button onClick={() => set글제목('여자 코트 추천')}></button>
+        <p>2월 17일 발행</p>
+      </div>
+      <div className='list'>
+        <h4>{글제목[1]}</h4>
+        <p>2월 17일 발행</p>
+      </div>
+      <div className='list'>
+        <h4>{글제목[2]}</h4>
+        <p>2월 17일 발행</p>
+      </div>
     </div>
   );
 }
